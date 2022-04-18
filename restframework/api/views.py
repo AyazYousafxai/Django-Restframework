5a204c0773ec7fb734e6b1ae377a60ea4cbbbc12
assistechsol <ceo@assistechsol.com>
2022-04-18 13:49:06 +0500

    add model

--- restframework/api/views.py
+++ restframework/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import json
+import re
+from django.http import JsonResponse
 
-# Create your views here.
+def api_home(request, *args, **kwargs):
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+
+    body = request.body  # bytes string of json data
+    print(body)
+    data = {}
+    # data = {
+    #     'message': 'Hello API',
+    #     'status': '200',
+    # }
+    try:
+        data = json.loads(body)
+    except:
+        pass
+
+    # print(data)
+    data['params'] = dict(request.GET)
+    data['header'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    print(data)
+    return JsonResponse(data)
